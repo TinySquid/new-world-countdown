d1fd5ea85e0ce71f24e9796f819f0786d9225380
Michael Nunes <tinysquid.git@gmail.com>
2021-08-12 17:44:27 -0700

    Refactor

--- server/index.js
+++ server/index.js
@@ -1,41 +1,42 @@
+// Imports
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-const { sendNotification } = require("./config/notification");
 
+const router = require("./routes/api");
+
+// Setup
 const app = express();
 const port = process.env.PORT || 8000;
 
+// Cors
+const whitelist = [process.env.CLIENT];
+
 const corsOptions = {
-	origin: "https://new-world-countdown.netlify.app",
+	origin: function (origin, callback) {
+		if (whitelist.indexOf(origin) !== -1) {
+			callback(null, true);
+		} else {
+			callback(new Error("Not allowed by CORS"));
+		}
+	},
 	optionsSuccessStatus: 200
 };
 
+if (process.env.NODE_ENV !== "production") {
+	app.use(cors());
+} else {
+	app.use(cors(corsOptions));
+}
+
 app.use(helmet());
-app.use(cors(corsOptions));
+
 app.use(express.json());
 
-app.post("/subscribe", (req, res) => {
-	try {
-		const { id, subscription } = req.body;
+// API Routing
+app.use("/api", router);
 
-		sendNotification(subscription);
-
-		// TODO Add user to subscriptions table
-
-		res.sendStatus(201);
-	} catch (e) {
-		console.log(e);
-	}
-});
-
-app.post("/unsubscribe", (req, res) => {
-	const { id } = req.body;
-
-	// TODO Remove user from subscriptions table
-	res.sendStatus(200);
-});
-
+// Start
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
 });
