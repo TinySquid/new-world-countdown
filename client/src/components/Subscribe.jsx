c719b984b4f3e828dbee43f521ad5f5ca34a31af
Michael Nunes <tinysquid.git@gmail.com>
2021-08-12 18:51:16 -0700

    Refactor subscriber component

--- client/src/components/Subscribe.jsx
+++ client/src/components/Subscribe.jsx
@@ -2,14 +2,19 @@ import { useState } from "react";
 
 import { urlBase64ToUint8Array } from "../utils/urlBase64ToUint8Array";
 
+import { unregister } from "../serviceWorkerRegistration";
+
 import { FiBell, FiBellOff } from "react-icons/fi";
 
 function Subscribe() {
 	// TODO Change to local storage hook
-	const [subscribed, setSubscribed] = useState(window.localStorage.getItem("subscribed"));
+	const [subscribed, setSubscribed] = useState(JSON.parse(window.localStorage.getItem("subscribed")));
 
 	async function registerNotifications() {
 		// Subscribe to push notifications
+		setSubscribed(true);
+		window.localStorage.setItem("subscribed", true);
+
 		const registration = await navigator.serviceWorker.ready;
 
 		const subscription = await registration.pushManager.subscribe({
@@ -17,30 +22,27 @@ function Subscribe() {
 			applicationServerKey: urlBase64ToUint8Array(process.env.REACT_APP_PUBLIC_VAPID_KEY)
 		});
 
-		const response = await fetch(`${process.env.REACT_APP_API}/subscribe`, {
+		await fetch(`${process.env.REACT_APP_API}/subscribe`, {
 			method: "POST",
 			body: JSON.stringify({ id: window.localStorage.getItem("id"), subscription: subscription }),
 			headers: { "content-type": "application/json" }
 		});
-
-		if (response.status === 201) {
-			setSubscribed(true);
-			window.localStorage.setItem("subscribed", true);
-		}
 	}
 
 	async function unregisterNotifications() {
 		// Unsubscribe from push notifications
-		const response = await fetch(`${process.env.REACT_APP_API}/unsubscribe`, {
+		setSubscribed(false);
+		window.localStorage.setItem("subscribed", false);
+
+		// Guarantee notifications stop locally
+		unregister();
+
+		// Remove subscriber from backend
+		await fetch(`${process.env.REACT_APP_API}/unsubscribe`, {
 			method: "POST",
 			body: JSON.stringify({ id: window.localStorage.getItem("id") }),
 			headers: { "content-type": "application/json" }
 		});
-
-		if (response.status === 200) {
-			setSubscribed(false);
-			window.localStorage.setItem("subscribed", false);
-		}
 	}
 
 	return (
